e0ad239562e75f31f23923111d32d0d2228b42f6
syahrulweb <sahrulloji05@gmail.com>
2025-09-25 17:36:48 +0700

    style chatbot

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Komputer-arrum/', // HARUS sesuai nama repo
+  base: '/ITCLUB-WP/', // HARUS sesuai nama repo
   server: {
     proxy: {
       '/api': {
