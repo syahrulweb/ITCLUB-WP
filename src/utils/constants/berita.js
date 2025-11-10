55b94940af05c1b52c173e782c2a6c17be2ef92c
syahrulweb <sahrulloji05@gmail.com>
2025-11-10 22:26:35 +0700

    up pertemuan 5

--- src/utils/constants/berita.js
+++ src/utils/constants/berita.js
@@ -1,59 +1,59 @@
 const berita = [
   {
     id: 1,
-    judul: "Pendaftaran Sekolah Dibuka!",
+    judul: "#",
     deskripsi:
       "Pendaftaran untuk tahun ajaran baru sudah dibuka. Ayo segera daftar sebelum kuota penuh!",
     gambar: import.meta.env.BASE_URL + "images/1.webp",
   },
   {
     id: 2,
-    judul: "Kegiatan Ekstrakurikuler Minggu Ini",
+    judul: "#",
     deskripsi:
       "Jangan lewatkan kegiatan ekstrakurikuler minggu ini, banyak kegiatan seru yang bisa diikuti.",
     gambar: import.meta.env.BASE_URL + "images/2.webp",
   },
   {
     id: 3,
-    judul: "Hasil Ujian Tengah Semester",
+    judul: "H#",
     deskripsi:
       "Pengumuman hasil UTS sudah diumumkan. Cek hasil ujian kalian melalui website sekolah.",
     gambar: import.meta.env.BASE_URL + "images/lab2.webp",
   },
   {
     id: 4,
-    judul: "Pertandingan Futsal",
+    judul: "#",
     deskripsi:
       "Saksikan pertandingan futsal antar kelas yang seru minggu ini.",
     gambar: import.meta.env.BASE_URL + "images/lab.webp",
   },
   {
     id: 5,
-    judul: "Pelajaran Tambahan",
+    judul: "#",
     deskripsi:
       "Jadwal pelajaran tambahan untuk persiapan ujian akhir sudah tersedia.",
     gambar: import.meta.env.BASE_URL + "images/3.webp",
   },
   {
     id: 6,
-    judul: "Upacara Hari Senin",
+    judul: "#",
     deskripsi:
       "Upacara bendera diikuti oleh seluruh siswa dengan khidmat.",
     gambar: import.meta.env.BASE_URL + "images/pekan222.webp",
   },
   {
     id: 7,
-    judul: "Pawai Kemerdekaan",
+    judul: "P#",
     deskripsi:
       "Siswa-siswi ikut memeriahkan pawai kemerdekaan dengan penuh semangat.",
-    gambar: import.meta.env.BASE_URL + "images/pawai.webp",
+    gambar: import.meta.env.BASE_URL + "images/10.webp",
   },
   {
     id: 8,
     judul: "Gambar 8",
     deskripsi:
       "kursus.",
-    gambar: import.meta.env.BASE_URL + "images/pass.webp",
+    gambar: import.meta.env.BASE_URL + "images/11.webp",
   },
 ];
 
